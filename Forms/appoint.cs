8246a53a7948fadd4267b4a8ad04a09e9e32bde2
User <User@DESKTOP-PK>
2023-10-28 09:59:20 +0500

    Add project files.

---
+++ Forms/appoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiH_VP_Project_.Forms
+{
+    public partial class appoint : Form
+    {
+        public appoint()
+        {
+            InitializeComponent();
+        }
+    }
+}
